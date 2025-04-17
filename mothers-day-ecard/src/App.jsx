20be3b7c0aac68f8801b3d13dd6ecb5433cc1bae
Shado <blovelace84@gmail.com>
2025-04-17 09:23:59 -0400

    New commit

---
+++ mothers-day-ecard/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import ECard from './components/ecard'
+
+function App() {
+  return (
+    <div className='app'>
+      <ECard />
+    </div>
+  )
+}
+
+export default App
---
+++ mothers-day-ecard/src/components/ecard.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const ECard = () => {
+    const [showCard, setShowCard] = useState(false);
+
+    const handleStartClick = () => {
+        setShowCard(true);
+    };
+
+    return(
+        <div className="title-screen">
+           {!showCard ? (
+            <div className="title-content">
+                <h1>Happy Mothers Day!</h1>
+                <p>Click below to view your e-card</p>
+                <button onClick={handleStartClick}>View Message</button>
+            </div>
+           ) : (
+            <div className="ecard-content">
+                <h1>Happy Mother's Day!</h1>
+                <p>Dear Mom,</p>
+                <p>Thank you for your endless love and support.</p>
+                <p>You are the heart of our family.</p>
+                <p>Wishing you a day filled with love and joy!</p>
+                <p>I will forever hold my mother's hand.</p>
+                <p>Love, Brittani</p>
+                <button onClick={() => setShowCard(false)}>Back</button>
+            </div>
+           )}
+        </div>
+    )
+}
+
+export default ECard;
